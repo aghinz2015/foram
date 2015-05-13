685355c0a714a69fcfb110f9d107a2314c535256
ErykZimonczyk <eryk.zimonczyk@gmail.com>
2015-05-13 22:51:06 +0200

    Initial app

---
+++ web.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Eryk on 2015-04-13.
+ */
+var gzippo = require('gzippo');
+var express = require('express');
+var morgan = require('morgan');
+var app = express();
+
+app.use(morgan('dev'));
+app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+app.listen(process.env.PORT || 5000);
