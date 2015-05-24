0b600bb93cd03f4133771f804162a6c6de92e2ec
ErykZimonczyk <eryk.zimonczyk@gmail.com>
2015-05-24 22:11:24 +0200

    FORAM-25 Test RailsAPI - http to https

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,7 +9,7 @@
  */
 angular.module('trunkApp')
   .controller('MainCtrl',['$scope','$http', function ($scope,$http) {
-    $http.get('http://foram-api.herokuapp.com/forams').success(function(data, status, headers, config) {
+    $http.get('https://foram-api.herokuapp.com/forams').success(function(data, status, headers, config) {
       $scope.forams = data;
       $scope.status = status;
     });
