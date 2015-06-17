6f144c2b9660bc3895049cea3e28b8d133784c7e
Lukasz Oliwa <oliwa@student.agh.edu.pl>
2015-06-17 02:13:50 +0200

    Fix chart data selector

--- app/scripts/controllers/charts.js
+++ app/scripts/controllers/charts.js
@@ -14,7 +14,7 @@ app.controller('ChartsCtrl',['$scope','DatasetService', function ($scope,Dataset
         $scope.chartEditingModeStatus = $scope.chartEditingMode ? 'Finish' : 'Edit chart';
   });
   $scope.colors = [];
-  $scope.currentData = "kx";
+  $scope.currentData = undefined;
   $scope.chartConfig = {
     colors: ["#2b908f", "#90ee7e", "#f45b5b", "#7798BF", "#aaeeee", "#ff0066", "#eeaaee",
       "#55BF3B", "#DF5353", "#7798BF", "#aaeeee"],
