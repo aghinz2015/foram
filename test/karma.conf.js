a062abe8536fa89aa4464fcf49c04ff2f8dc55c8
luki9316 <luki9316@WINDOWSLIVE.com>
2015-10-06 21:35:32 +0200

    Use highcharts as bower component and fix angular version

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -36,6 +36,9 @@ module.exports = function(config) {
       'bower_components/highcharts-ng/dist/highcharts-ng.js',
       'bower_components/jquery-ui/jquery-ui.js',
       'bower_components/ngDialog/js/ngDialog.js',
+      'bower_components/highcharts/highcharts.js',
+      'bower_components/highcharts/highcharts-more.js',
+      'bower_components/highcharts/modules/exporting.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
