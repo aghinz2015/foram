7d9fa56d8b89a5397362f202b187fce11818750b
Bartosz Zurkowski <bartosz.zurkowski@codesthq.com>
2015-10-18 02:24:37 +0200

    Fix offcanvas menu

--- app/assets/js/sidebar.js
+++ app/assets/js/sidebar.js
@@ -1,13 +1,15 @@
-function sideNav() {
-  if ($(window).width() < 769) {
-    $('.off-canvas-wrap').removeClass('move-right');
-    $('.left-off-canvas-toggle').show();
-  } else {
-    $('.off-canvas-wrap').addClass('move-right');
-    $('.left-off-canvas-toggle').hide();
+$(document).ready(function() {
+  function sideNav() {
+    if ($(window).width() < 769) {
+      $('.off-canvas-wrap').removeClass('move-right');
+      $('.left-off-canvas-toggle').show();
+    } else {
+      $('.off-canvas-wrap').addClass('move-right');
+      $('.left-off-canvas-toggle').hide();
+    }
   }
-}
 
-$(window).resize(function() {
-  sideNav();
+  $(window).resize(function() {
+    sideNav();
+  });
 });
