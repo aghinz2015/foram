df3aa777c6f0356b74b9a7d8b3303851d4ee2a6d
luki9316 <luki9316@windowslive.com>
2015-10-18 14:36:04 +0200

    Remove console log

--- app/scripts/controllers/charts.js
+++ app/scripts/controllers/charts.js
@@ -118,9 +118,11 @@ app.controller('ChartsCtrl', ['$scope', 'ConfigService', 'ForamAPIService', 'ngD
       ForamAPIService.getGenerations(flatParams).then(function (response) {
         $scope.chartParams = {};
         generations = response.data.result;
+        
         for(var i = 0; i < generations.grouping_parameter.values.length; i++) {
           groupSize[generations.grouping_parameter.values[i]] = generations.grouping_parameter.sizes[i];
         }
+        
         $scope.chart.xAxis.categories = generations.grouping_parameter.values;
         $scope.chart.xAxis.title = {};
         $scope.chart.xAxis.title.text = generations.grouping_parameter.name;
@@ -230,7 +232,6 @@ app.controller('ChartsCtrl', ['$scope', 'ConfigService', 'ForamAPIService', 'ngD
             var s = this.x + '<br/>';
             $.each(this.points, function(i, point) {
               s += '<span style="color:'+point.series.color+'">'+point.series.name+'</span>: ';
-              console.log(point);
               if(point.point.low === undefined) {
                  s += '<b>'+point.y+'</b><br/>';
               } else {
