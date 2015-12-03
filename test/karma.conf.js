238bd4dd171cc8aed0df10a065333f317a128a77
Bartosz Zurkowski <bartosz.zurkowski@codesthq.com>
2015-12-04 00:30:39 +0100

    Install clipboard.js

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -32,6 +32,7 @@ module.exports = function(config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
+      'bower_components/clipboard/dist/clipboard.js',
       'bower_components/threejs/build/three.js',
       'bower_components/threejs-obj-exporter/OBJExporter.js',
       'bower_components/threejs-trackball-controls/TrackballControls.js',
